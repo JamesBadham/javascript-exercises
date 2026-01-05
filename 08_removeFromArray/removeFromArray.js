c77f260be66e650b661a674230dd75362a75a272
JamesBadham <15140053+JamesBadham@users.noreply.github.com>
2026-01-05 16:09:16 +0000

    Complete 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArr, ...elementsToRemove) {
+    return inputArr.filter(item => !elementsToRemove.includes(item));   
 };
 
 // Do not edit below this line
