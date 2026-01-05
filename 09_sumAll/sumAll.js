4297b2c2bb0969f3b075297d7d3ee0f76ac3a800
JamesBadham <15140053+JamesBadham@users.noreply.github.com>
2026-01-05 16:20:12 +0000

    Complete 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    if (first < 0 || second < 0 || !Number.isInteger(first) || !Number.isInteger(second)) {
+        return 'ERROR';
+    }
+    let start = first;
+    let end = second;
+    let sum = 0;
+    if (first > second) {
+        start = second;
+        end = first;
+    }
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;        
 };
 
 // Do not edit below this line
